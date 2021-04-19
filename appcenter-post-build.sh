2b4a5646416f8955972c3860159538808ba1c3fe
Luis Colina <luiscolina@Luiss-MacBook-Pro.local>
2021-04-19 09:13:40 +0200

    post build added

---
+++ appcenter-post-build.sh
@@ -0,0 +1,12 @@
+if [ "$APPCENTER_BRANCH" = "dev" ]; then
+
+    export RN_SRC_EXT=e2e.js
+
+    if [ "$OS" = "iOS" ]; then
+        echo "iOS: Executing Detox tests..."
+        npx detox test --configuration ios.sim.release --cleanup --loglevel verbose
+    else
+        echo "Android: Executing Detox tests..."
+        npx detox test --configuration android.emu.release --headless --loglevel verbose
+    fi
+fi
