398a301c5a432f98e8d067b8ab14a6aaf03e2cbb
Luis Colina <luiscolina@Luiss-MacBook-Pro.local>
2021-04-20 15:50:18 +0200

    test changes

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,16 +1,13 @@
-beforeAll(async () => {
+beforeAll(async done => {
   await detox.init(undefined, { launchApp: false });
   await device.launchApp();
   await device.reloadReactNative();
-});
-
-afterAll(async done => {
-  //allows Jest to exit successfully.
   done();
 });
 
 describe('App', () => {
-  it('should show the step one message', async () => {
+  it('should show the step one message', async done => {
     await expect(element(by.id('stepOne'))).toBeVisible();
+    done();
   });
 });
