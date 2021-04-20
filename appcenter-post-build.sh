9712dc5c09cb0bbec6c6e2bca4aecab35711544d
Luis Colina <luiscolina@Luiss-MacBook-Pro.local>
2021-04-20 11:23:59 +0200

    config settings new test

--- appcenter-post-build.sh
+++ appcenter-post-build.sh
@@ -1,12 +1,4 @@
 if [ "$APPCENTER_BRANCH" = "master" ]; then
 
-    export RN_SRC_EXT=e2e.js
-
-    if [ "$OS" = "iOS" ]; then
-        echo "iOS: Executing Detox tests..."
-        npx detox test --configuration ios.sim.release --cleanup --loglevel verbose
-    else
-        echo "Android: Executing Detox tests..."
-        npx detox test --configuration android.emu.release --headless --loglevel verbose
-    fi
+   echo "TESTING"
 fi
--- appcenter-pre-build.sh
+++ appcenter-pre-build.sh
@@ -1,33 +1,48 @@
-if [ "$APPCENTER_BRANCH" = "master" ]; then
+#!/usr/bin/env bash
 
-    export RN_SRC_EXT=e2e.js
-
-    if [ "$OS" = "iOS" ]; then
-        echo "iOS: Installing cocoapods..."
-        npm run pod-install
-
-        echo "iOS: Building the project for Detox tests..."
-        npx detox build --configuration ios.sim.release --loglevel verbose
-
-        echo "iOS: Executing Detox tests..."
-        npx detox test --configuration ios.sim.release --cleanup --loglevel verbose
-    else
-
-        echo "Android: Building the project for Detox tests..."
-        npx detox build --configuration android.emu.release --loglevel verbose
-
-        echo "Android: Downloading android image..."
-        /Users/runner/Library/Android/sdk/tools/bin/sdkmanager "system-images;android-27;default;x86_64"
-
-        echo "Android: Accepting licenses for android images.."
-        echo N | /Users/runner/Library/Android/sdk/tools/bin/sdkmanager --licenses
-        touch /Users/runner/.android/repositories.cfg
-
-        echo "Android: Creating AVD.."
-        echo no | /Users/runner/Library/Android/sdk/tools/bin/avdmanager create avd -n Pixel_API_27_AOSP -d pixel --package "system-images;android-27;default;x86_64"
-        cp app-center/config.ini /Users/runner/.android/avd/Pixel_API_27_AOSP.avd/config.ini
-
-        echo "Android: Executing Detox tests..."
-        npx detox test --configuration android.emu.release --headless --loglevel verbose
-    fi
+if [ -z "$OS" ]
+then
+    echo "You need define the OS variable in App Center with values android or ios"
+    exit
 fi
+
+if [ "$OS" == "android" ]
+then
+    echo "Setup Android simulator"
+    SIMULATOR_IMAGE="system-images;android-28;google_apis;x86"
+    SIMULATOR_NAME="Pixel_XL_API_28"
+
+    ANDROID_HOME=~/Library/Android/sdk
+    ANDROID_SDK_ROOT=~/Library/Android/sdk
+    ANDROID_AVD_HOME=~/.android/avd
+    PATH="$ANDROID_HOME/tools:$ANDROID_HOME/tools/bin:$ANDROID_HOME/platform-tools:$PATH"
+
+    echo "Accepts all sdk licences"
+    yes | sdkmanager --licenses
+
+    echo "Download Simulator Image"
+    sdkmanager --install "$SIMULATOR_IMAGE"
+
+    echo "Create Simulator '$SIMULATOR_NAME' with image '$SIMULATOR_IMAGE'"
+    echo "no" | avdmanager --verbose create avd --force --name "$SIMULATOR_NAME" --device "pixel" --package "$SIMULATOR_IMAGE" --tag "google_apis" --abi "x86"
+
+    DETOX_CONFIG=android.emu.release
+else
+    echo "Install AppleSimUtils"
+
+    brew tap wix/brew
+    brew update
+    brew install applesimutils
+
+    echo "Install pods "
+    cd ios; pod install; cd ..
+
+    DETOX_CONFIG=ios.sim.release
+fi
+
+
+echo "Building the project for Detox tests..."
+npx detox build --configuration "$DETOX_CONFIG"
+
+echo "Executing Detox tests..."
+npx detox test --configuration "$DETOX_CONFIG" --cleanup
