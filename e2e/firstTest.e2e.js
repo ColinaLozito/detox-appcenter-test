b55239998edfc66aa58936b29db1cac07249d543
Luis Colina <luiscolina@Luiss-MacBook-Pro.local>
2021-04-20 15:06:08 +0200

    jest settings changes

--- e2e/firstTest.e2e.js
+++ e2e/firstTest.e2e.js
@@ -1,16 +1,15 @@
-describe('App', () => {
-  beforeAll(async () => {
-    await detox.init(undefined, { launchApp: false });
-    await device.launchApp();
-  });
-  beforeEach(async () => {
-    await device.launchApp();
-    await device.reloadReactNative();
-  });
-  afterEach(async () => {
-    await device.terminateApp();
-  });
+beforeAll(async () => {
+  await detox.init(undefined, { launchApp: false });
+  await device.launchApp();
+  await device.reloadReactNative();
+});
 
+afterAll(async done => {
+  //allows Jest to exit successfully.
+  done();
+});
+
+describe('App', () => {
   it('should show the step one message', async () => {
     await expect(element(by.id('stepOne'))).toBeVisible();
   });
